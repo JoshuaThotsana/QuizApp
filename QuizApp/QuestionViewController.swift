ed9a2b96c49b979722c5510275a75f039cb02153
Joshua Thotsana Mabotsa <joshua.mabotsa@umuzi.org>
2020-12-11 01:43:34 +0200

    Testing View Controllers in Swift

--- QuizApp/QuestionViewController.swift
+++ QuizApp/QuestionViewController.swift
@@ -4,18 +4,37 @@
 //
 //  Created by Thotsana Mabotsa on 2020/12/11.
 //
-
+import Foundation
 import UIKit
 
-class QuestionViewController: UIViewController {
+class QuestionViewController: UIViewController, UITableViewDataSource {
 
+	@IBOutlet weak var tableView: UITableView!
 	@IBOutlet weak var headerLabel: UILabel!
 	
 	private var question: String = ""
+	private var options: [String] = []
 	
-	convenience init(question: String) {
+	convenience init(question: String, options: [String]) {
 		self.init()
 		self.question = question
+		self.options = options
+	}
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		headerLabel.text = question
+	}
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		options.count
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = UITableViewCell()
+		cell.textLabel?.text = options[indexPath.row]
+		return cell
 	}
 
 }
